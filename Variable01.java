2d958383adee37d408df4a67e4815da064e4ffbc
Muhammad Nurradit Fatahillah <2411016310017@mhs.ulm.ac.id>
2024-09-21 18:12:40 +0700

    Add files via upload

---
+++ Variable01.java
@@ -0,0 +1,21 @@
+public class Variable01 {
+
+    public static void main(String[] args) {
+        // deklaraasi var
+        String nama;
+        int umur;
+        // input nilai
+
+        nama = "Muhammad";
+        
+        // output ke layar
+        System.out.println("Nama: " + nama);
+
+        nama = "Nurradit Fatahillah";
+        System.out.println("Nama: " + nama);
+
+        
+        umur = 18;
+        System.out.println("Umur: " + umur);
+    }
+}
